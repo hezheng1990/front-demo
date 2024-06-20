ea22f54d71c1088aa2ea7b5698438a7a63f30347
muqingkun <muqingkun@jtexpress.com>
2024-06-20 18:43:53 +0800

    feat: 样式微调

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Description:
+ * @Author: muqingkun
+ * @Date: 2024-06-20 16:33:36
+ * @LastEditTime: 2024-06-20 18:32:30
+ * @LastEditors: muqingkun
+ * @Reference:
+-->
 <template>
   <div id="app">
 <!--    <img src="./assets/logo.png">-->
@@ -20,5 +28,42 @@ export default {
 </script>
 
 <style>
+html {
+  background: #fffbf6;
+}
+/* 滚动条整体 */
+::-webkit-scrollbar {
+    width: 12px;  /* 垂直滚动条宽度 */
+    height: 12px; /* 水平滚动条高度 */
+}
+
+/* 滚动条轨道 */
+::-webkit-scrollbar-track {
+    background: #f1f1f1;
+    border-radius: 6px;
+}
+
+/* 滚动条滑块 */
+::-webkit-scrollbar-thumb {
+    background: #c1c1c1;
+    border-radius: 6px;
+}
+
+/* 滚动条滑块悬停 */
+::-webkit-scrollbar-thumb:hover {
+    background: #8f8f8f;
+}
+
+ul {
+    margin: 0;            /* 清除默认外边距 */
+    padding: 0;           /* 清除默认内边距 */
+    list-style: none;     /* 清除默认列表样式 */
+}
+
+li {
+    margin: 0;            /* 清除默认外边距 */
+    padding: 0;           /* 清除默认内边距 */
+}
+
 
 </style>
